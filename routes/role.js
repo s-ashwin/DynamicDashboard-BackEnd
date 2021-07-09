65573a21005e8e8628d027f9a66a07605dec0092
s-ashwin <ashwin.sivanagarajan@gmail.com>
2021-07-09 19:05:50 +0530

    updated create role route

--- routes/role.js
+++ routes/role.js
@@ -1,9 +1,11 @@
 var express = require('express')
+const { isAdmin, isSignedin } = require('../controllers/auth')
 var router = express.Router()
-
+const { getUserById } = require('../controllers/user')
 const {createRole} = require("../controllers/role")
 
-router.post("/role/create",createRole)
+router.param('userId', getUserById)
+router.post("/role/create/:userId",isSignedin, isAdmin ,createRole)
 
 module.exports=router
 

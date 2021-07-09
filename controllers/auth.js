e5166a53017a8d01bbd31b4d2b31780fb13051d4
s-ashwin <ashwin.sivanagarajan@gmail.com>
2021-07-09 11:51:20 +0530

    created signout route

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,3 +54,10 @@ exports.signin = (req, res)=> {
 
     })
 }
+
+exports.signout = (req, res)=> {
+    res.clearCookie("token")
+    res.json({
+        message:"Signed Out"
+    })
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var router = express.Router()
 
-const {signup, signin} = require("../controllers/auth")
+const {signup, signin, signout} = require("../controllers/auth")
 
 router.post("/signup",signup)
 router.post("/signin",signin)
+router.get("/signout",signout)
 
 module.exports=router
 

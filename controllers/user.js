8c40b5e10a33ee40c1f9f430dffb0ebdd77996b8
s-ashwin <ashwin.sivanagarajan@gmail.com>
2021-07-10 12:01:51 +0530

    created more e routes - user

--- controllers/user.js
+++ controllers/user.js
@@ -12,9 +12,47 @@ exports.getUserById=(req,res,next,id)=>{
     })
 }
 
+exports.findUserById=(req,res,next,id)=>{
+    User.findOne({_id:id},{_id:1}).exec((err,user)=>{
+        if(err || !user){
+            return res.status(400).json({
+                error:"No User Found"
+            })
+        }
+        req.user=user
+        next()
+    })
+}
+
+exports.getAUser = (req,res)=>{
+    User.findOne({_id:req.user._id},{salt:0, encry_password:0, __v:0})
+        .populate('role', "name")
+        .exec((err,user)=>{
+            if(err || !user){
+                return res.status(400).json({
+                    error:"No Users Found"
+                })
+            }
+            res.json(user)
+        })
+}
+
+exports.getAllUsers = (req,res)=>{
+    User.find({},{salt:0, encry_password:0, __v:0})
+        .populate('role', "name")
+        .exec((err,user)=>{
+            if(err || !user){
+                return res.status(400).json({
+                    error:"No Users Found"
+                })
+            }
+            res.json(user)
+        })
+}
+
 exports.updateUser=(req,res)=>{
     User.findByIdAndUpdate(
-        {_id: req.profile._id},
+        {_id: req.user._id},
         {$set: req.body},
         {new:true, useFindAndModify:false},
         (err,user)=>{
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const { isSignedin, isAdmin } = require('../controllers/auth')
-const { getUserById, updateUser } = require('../controllers/user')
+const { getUserById, updateUser, getAllUsers, getAUser, findUserById } = require('../controllers/user')
 const router = express.Router()
 
 router.param('userId', getUserById)
-router.put("/user/update/:userId",isSignedin, isAdmin , updateUser)
+router.param('id', findUserById)
+router.get("/user/getall/:userId",isSignedin, isAdmin, getAllUsers)
+router.get("/user/get/:userId/:id",isSignedin, isAdmin, getAUser)
+router.put("/user/update/:userId/:id",isSignedin, isAdmin , updateUser)
 
 module.exports = router

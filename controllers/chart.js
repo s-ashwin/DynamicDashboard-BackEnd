18d32baf521f733140c19e85156f5dd4198c8a54
s-ashwin <ashwin.sivanagarajan@gmail.com>
2021-07-10 13:44:39 +0530

    created more e routes - chart

--- controllers/chart.js
+++ controllers/chart.js
@@ -13,6 +13,47 @@ exports.getChartById=(req,res,next,id)=>{
     })
 }
 
+exports.getAllCharts = (req,res)=>{
+    Chart.find({})
+        .populate("roles")
+        .exec((err,charts)=>{
+            if(err || !charts){
+                return res.status(400).json({
+                    error:"Error in getting all charts"
+                })
+            }
+            res.json(charts)
+        })
+}
+
+exports.getAChart = (req,res)=>{
+    Chart.findById(req.chart._id)
+        .exec((err,chart)=>{
+            if(err || !chart){
+                return res.status(400).json({
+                    error:"Chart not found"
+                })
+            }
+            res.json(chart)
+        })
+}
+
+exports.getMyCharts = (req,res)=>{
+    Chart.find({})
+    .populate({path:"roles", match:{name:req.profile.role.name}})
+    .exec((err,charts)=>{
+        if(err || !charts){
+            return res.status(400).json({
+                error:"Error in getting all charts"
+            })
+        }
+        mycharts = charts.filter(function(chart) {
+            return chart.roles[0]; 
+        });
+        res.json(mycharts)
+    })
+}
+
 exports.createChart = (req,res)=>{
     const chart = new Chart(req.body)
 
--- routes/chart.js
+++ routes/chart.js
@@ -2,12 +2,15 @@ const express = require('express')
 const { isAdmin, isSignedin } = require('../controllers/auth')
 const router = express.Router()
 const { getUserById } = require('../controllers/user')
-const { getChartById, createChart, updateChart } = require('../controllers/chart')
+const { getChartById, createChart, updateChart, getAllCharts, getAChart, getMyCharts } = require('../controllers/chart')
 
 router.param('userId', getUserById)
 router.param('chartId', getChartById)
+router.get("/chart/mycharts/:userId", isSignedin ,getMyCharts)
+router.get("/chart/getall/:userId", isSignedin, isAdmin ,getAllCharts)
+router.get("/chart/get/:userId/:chartId", isSignedin, isAdmin ,getAChart)
 router.post("/chart/create/:userId",isSignedin, isAdmin ,createChart)
-router.post("/chart/update/:userId/:chartId",isSignedin, isAdmin ,updateChart)
+router.put("/chart/update/:userId/:chartId",isSignedin, isAdmin ,updateChart)
 
 module.exports=router
 

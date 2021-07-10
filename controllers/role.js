c7d17239529334cbbc9eddc0e5b2c9c4862255c3
s-ashwin <ashwin.sivanagarajan@gmail.com>
2021-07-10 12:56:53 +0530

    created more e routes - role

--- controllers/role.js
+++ controllers/role.js
@@ -1,5 +1,33 @@
 const Role = require("../models/role")
 
+exports.getRoleById=(req,res,next,id)=>{
+    Role.findById(id).exec((err,role)=>{
+        if(err || !role){
+            return res.status(400).json({
+                error:"No Role Found"
+            })
+        }
+        req.role=role
+        next()
+    })
+}
+
+exports.updateRole = (req,res)=>{
+    Role.findByIdAndUpdate(
+        {_id: req.role._id},
+        {$set: req.body},
+        {new:true, useFindAndModify:false},
+        (err,role)=>{
+            if(err || !role){
+                return res.status(400).json({
+                    error:"Update Unsuccessful"
+                })
+            }
+            res.json(role)
+        }
+    )
+}
+
 exports.createRole = (req,res)=>{
     const role = new Role(req.body)
     role.save((err,name)=>{
@@ -8,4 +36,16 @@ exports.createRole = (req,res)=>{
         }
         res.json({name})
     })
+}
+
+exports.getAllRole = (req,res)=>{
+    Role.find()
+        .exec((err,roles)=>{
+            if(err || !roles){
+                return res.status(400).json({
+                    error:"Error in getting all roles"
+                })
+            }
+            res.json(roles)
+        })
 }
--- routes/role.js
+++ routes/role.js
@@ -2,10 +2,13 @@ var express = require('express')
 const { isAdmin, isSignedin } = require('../controllers/auth')
 var router = express.Router()
 const { getUserById } = require('../controllers/user')
-const {createRole} = require("../controllers/role")
+const {createRole, getAllRole, getRoleById, updateRole} = require("../controllers/role")
 
 router.param('userId', getUserById)
+router.param('roleId', getRoleById)
 router.post("/role/create/:userId",isSignedin, isAdmin ,createRole)
+router.get("/role/getall/:userId",isSignedin, isAdmin ,getAllRole)
+router.put("/role/update/:userId/:roleId",isSignedin, isAdmin ,updateRole)
 
 module.exports=router
 
